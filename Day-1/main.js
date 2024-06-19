c21e79880b01a5cff6863ef44bc8face8b50cc34
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-06-19 20:56:31 +0530

    some js basics

---
+++ Day-1/main.js
@@ -0,0 +1,43 @@
+// console.log("hello world!")
+
+// const a = 1;
+// a = 2
+// console.log(a)
+
+// let fName = "ashwini"
+// let age = 26
+// let isLearning = true
+
+// console.log("my name is : " + fName + " and age is " +age)
+// if (isLearning){
+//     console.log(fName + " is Learning")
+// } else {
+//     console.log(fName + " is not Learning")
+// }
+
+// let answer = 0
+
+// for (i=0; i<=10; i = i+1){
+//     answer = answer + i
+// }
+// console.log(answer)
+
+// let fname = "ashwini"
+// let lname = "paraye"
+// let isFemale = true
+
+// console.log("hello " + fname +" " +lname)
+// if (isFemale) {
+//     console.log(fname + " is female")   
+// } else {
+//     console.log(fname + " is male")   
+// }
+
+
+let num = 0
+let count = 0
+for (let i = 0; i <= 5; i++) {
+    count = count + i
+}
+
+console.log(count)
