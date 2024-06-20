eb36227887ce5147e867b724dc22c0b89b31ea34
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-06-20 23:03:19 +0530

    numbers manipulation

--- 02/main.js
+++ 02/main.js
@@ -48,3 +48,54 @@
 // const str = "ashWIni"
 // console.log(str.toLowerCase());
 
+// console.log(parseInt("23"));
+// console.log(parseInt("24as"));
+// console.log(parseInt("2sdfsdf3"));
+// console.log(parseInt("23.34"));
+
+
+// console.log(parseFloat("23.3d4"));
+// console.log(parseFloat("24as"));
+// console.log(parseFloat("2sdfsdf3"));
+// console.log(parseFloat("23.34"));
+
+
+// const initialArr = [1,2,3,4]
+// initialArr.push(44)
+// console.log(initialArr);
+
+// const initialArr = [1,2,3,4]
+// initialArr.pop()
+// console.log(initialArr);
+
+// const initialArr = [1,2,3,4]
+// initialArr.shift()
+// console.log(initialArr);
+
+// const initialArr = [1,2,3,4]
+// initialArr.unshift(0)
+// console.log(initialArr);
+
+// const initialArr = [1,2,3,4]
+// const secondArr = [8, 8]
+// // const val = initialArr.concat(secondArr)
+// // console.log(val);
+// console.log(initialArr.concat(secondArr));
+
+
+// const initialArr = [1,2,3,4]
+// const secondArr = [8, 8]
+
+// for (let i = 0; i < initialArr.length; i++) {
+//    console.log(initialArr[i]);
+// }
+
+
+const initialArr = [1,2,3,4]
+const secondArr = [8, 8]
+
+function logThing(str) {
+    console.log(str);
+}
+
+initialArr.forEach(logThing)
