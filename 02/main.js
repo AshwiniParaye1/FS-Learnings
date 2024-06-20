0df3b906a5538c108755b6d32ba7d1a37d9cecb6
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-06-20 21:13:04 +0530

    string manipulation

---
+++ 02/main.js
@@ -0,0 +1,50 @@
+// function getLength(str) {
+//     console.log("original string : ", str);
+//     console.log("length of string : ", str.length);
+// }
+
+// getLength("Hey ashwini ")
+
+
+// function findIndexOf(str,target) {
+//     console.log("original string : ", str);
+//     console.log("length of string : ", str.indexOf(target));
+// }
+
+// findIndexOf("hello world", "world")
+
+
+// function findLastIndexOf(str,target) {
+//     console.log("original string : ", str);
+//     console.log("length of string : ", str.lastIndexOf(target));
+// }
+
+// findLastIndexOf("hello world world", "world")
+
+// let ans = "ashwini paraye".substr(2,5)
+// let ans2 = "ashwini paraye".slice(2,5)
+// console.log(ans);
+// console.log(ans2);
+
+
+// const str = "hello world"
+// console.log(str.replace("world", "js"));
+
+
+// const value = "hi, my, name, is, ashwini"
+// const words = value.split(",")
+// console.log(words);
+
+
+// const val = "        ashwini          paraye     "
+// console.log(val.trim());
+
+
+
+// const str = "ashwini"
+// console.log(str.toUpperCase());
+
+
+// const str = "ashWIni"
+// console.log(str.toLowerCase());
+
