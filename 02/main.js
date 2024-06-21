7f09058b9586811eba6a962426bcc303e7926c01
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-06-21 18:18:19 +0530

    date

--- 02/main.js
+++ 02/main.js
@@ -120,3 +120,34 @@
 
 // dog.speak() //call function on object
 // cat.speak()
+
+const currentDate = new Date()
+
+// console.log(currentDate.getDate());
+// console.log(currentDate.getMonth() + 1);
+// console.log(currentDate.getFullYear());
+// console.log(currentDate.getHours());
+// console.log(currentDate.getMinutes());
+// console.log(currentDate.getSeconds());
+
+
+
+// console.log("time in miliseconds since 1970 : ",currentDate.getTime());  //epoch timestamp
+
+// function calculateSum(n) {
+//     let a = 0
+//     for (let i = 0; i < n; i++) {
+//      a = a + i
+//     }
+//     return a
+// }
+
+// const beforeDate = new Date()
+// const beforeTimeInMs = beforeDate.getTime()
+
+// calculateSum(100000)
+
+// const afterDate = new Date()
+// const afterTimeInMs = afterDate.getTime()
+
+// console.log(afterTimeInMs - beforeTimeInMs);
