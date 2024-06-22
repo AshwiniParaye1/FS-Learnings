5105aa3fac76b54cd77e65239f106db595232e02
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-06-22 20:04:30 +0530

    async functions

---
+++ 03/main.js
@@ -0,0 +1,58 @@
+// function findSum(n) {
+//     let ans = 0
+//     for (let i=0; i<n; i++){
+//         ans += i
+//     }
+//     return ans
+// }
+
+// function findSumTillN() {
+//     console.log(findSum(100));
+// }
+
+// setTimeout(findSumTillN, 1000)
+// console.log("hello");
+
+// const { log } = require("console");
+// const fs = require("fs")
+// fs.readFile("ex.txt", "utf-8", function (err, data) {
+//     console.log(data);
+// })
+
+// console.log("hello");
+
+// let a=0;
+// for(let i=0; i<100000000000; i++){
+//     a++
+// }
+
+// console.log("hi 2");
+
+console.log("hi");
+
+setTimeout(function timeout() {
+  console.log("first set timeout function");
+}, 8000);
+
+setTimeout(function timeout() {
+  console.log("second set timeout function");
+}, 5000);
+
+let a = 0;
+for (let i = 0; i < 10; i++) {
+  a = a + 1;
+}
+
+console.log(a);
+
+// console.log("hi");
+
+// function calculateSum() {
+//   let a = 0;
+//   for (let i = 0; i < 10; i++) {
+//     a = a + 1;
+//   }
+//   console.log(a);
+// }
+
+// calculateSum();
