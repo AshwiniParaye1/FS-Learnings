8629004ab9c102687fec72d00c2f9c22059de4c4
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-06-29 16:53:24 +0530

    callbacks revision

---
+++ 04/main.js
@@ -0,0 +1,15 @@
+function square(n) {
+  return n * n;
+}
+function cube(n) {
+  return n * n * n;
+}
+
+function sumOfSomething(a, b, fn) {
+  let square1 = fn(a);
+  let square2 = fn(b);
+  return square1 + square2;
+}
+
+let ans = sumOfSomething(1, 2, square);
+console.log(ans);
