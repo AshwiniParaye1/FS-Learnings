4ffe3b3e35896fc62c5a559fac5a3018c615d8e0
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-07-01 20:45:29 +0530

    created different methods-get, post

--- 05/index.js
+++ 05/index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.send("<b>Hey There!</b>");
+});
+
+app.get("/jsonObj", function (req, res) {
+  res.json({
+    FirstName: "Ashwini",
+    LastName: "Paraye",
+  });
+});
+app.post("/conversation", function (req, res) {
+  console.log("req body is ==== ", req.body);
+  //   console.log(req.headers);
+  res.send({
+    msg: "some message",
+  });
 });
 
 app.listen(port, function () {
