fdc33f2d545a02e073da8d05e239d3817d25afc3
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-07-12 18:22:01 +0530

    solved the assignment

---
+++ dom/calculateInterest/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.get("/sum", (req, res) => {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  const sum = a + b;
+  res.send(sum.toString());
+});
+
+app.get("/interest", (req, res) => {
+  const principal = parseInt(req.query.principal);
+  const rate = parseInt(req.query.rate);
+  const time = parseInt(req.query.time);
+  const interest = (principal * rate * time) / 100;
+  // const total = principal + interest;
+  // res.send({
+  //   total: total,
+  //   interest: interest,
+  // });
+  res.send(interest.toString());
+});
+
+app.listen(3000, () => {
+  console.log(`Calculate Interest server listening on port 3000`);
+});
