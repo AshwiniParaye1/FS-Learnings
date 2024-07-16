02e64877df71b71db2a9c6cc37c4d79a59a85b6f
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-07-16 16:57:14 +0530

    created a badge comp jsx-runtime

--- react/vite-project/src/App.jsx
+++ react/vite-project/src/App.jsx
@@ -1,20 +1,29 @@
 // import { useState } from "react";
 import "./App.css";
 // import ChildComponent from "./ChildComponent";
-import ParentComponent from "./ParentComponent";
-import ReactElement from "./ReactElement";
+// import ParentComponent from "./ParentComponent";
+// import ReactElement from "./ReactElement";
+import { jsx } from "react/jsx-runtime";
+import Badge from "./Badge";
 
 function App() {
   // const [count, setCount] = useState(0);
-  return (
-    <div>
-      {/* <button onClick={() => setCount(() => Math.random())}>
-        count is {count}
-      </button> */}
-      <ParentComponent />
-      <ReactElement />
-    </div>
-  );
+  return jsx(Badge, {
+    name: "Ashwini Paraye",
+    handle: "AshwiniParaye1",
+    img: "https://avatars.githubusercontent.com/u/104343839?s=400&u=e8730957db05636526e4478f7f414ab2459e61ba&v=4",
+    link: "https://github.com/AshwiniParaye1",
+  });
+  // (
+  //   <div>
+  //     {/* <button onClick={() => setCount(() => Math.random())}>
+  //       count is {count}
+  //     </button> */}
+  //     {/* <ParentComponent /> */}
+  //     {/* <ReactElement /> */}
+
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ react/vite-project/src/Badge.jsx
@@ -0,0 +1,26 @@
+import { jsx } from "react/jsx-runtime";
+
+export default function Badge({ name, handle, img, link }) {
+  return jsx("div", {
+    className: "badge",
+    children: [
+      jsx("img", {
+        alt: name,
+        src: img,
+      }),
+      jsx("div", {
+        children: [
+          jsx("h4", {
+            children: name,
+          }),
+          jsx("p", {
+            children: ["@", handle],
+          }),
+          jsx("a", {
+            children: ["View Profile: ", link],
+          }),
+        ],
+      }),
+    ],
+  });
+}
