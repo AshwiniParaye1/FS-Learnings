f44551ac6179ff64f627abb4e53e7773cfae87e4
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-07-24 19:36:44 +0530

    created slider

--- react/useReducer/src/Slider.jsx
+++ react/useReducer/src/Slider.jsx
@@ -1,5 +1,25 @@
-// import React from 'react'
+/* eslint-disable react/prop-types */
+import * as React from "react";
 
-export default function Slider() {
-  return <div>Slider</div>;
+export default function Slider({ min, max, onChange }) {
+  const [value, setValue] = React.useState(min);
+
+  const handleChange = (event) => {
+    const newValue = Number(event.target.value);
+    setValue(newValue);
+    onChange(newValue);
+  };
+
+  return (
+    <div>
+      <input
+        type="range"
+        min={min}
+        max={max}
+        value={value}
+        onChange={handleChange}
+      />
+      <p>Step: {value}</p>
+    </div>
+  );
 }
