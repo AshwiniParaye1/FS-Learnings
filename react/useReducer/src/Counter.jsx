8991fcc17af04a991b19d7b4a501abcf8cfcda41
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-07-24 19:37:37 +0530

    modified counter to use action type

--- react/useReducer/src/Counter.jsx
+++ react/useReducer/src/Counter.jsx
@@ -2,54 +2,56 @@ import * as React from "react";
 import Slider from "./Slider";
 
 function reducer(state, action) {
-  if (action === "increment") {
+  if (action.type === "increment") {
     return {
-      count: state.count + 1,
+      count: state.count + state.step,
       step: state.step,
     };
-  } else if (action === "decrement") {
+  } else if (action.type === "decrement") {
     return {
-      count: state.count - 1,
+      count: state.count - state.step,
       step: state.step,
     };
-  } else if (action === "reset") {
+  } else if (action.type === "reset") {
     return {
       count: 0,
       step: state.step,
+    };
+  } else if (action.type === "updateStep") {
+    return {
+      count: state.count,
+      step: action.step,
     };
   } else {
     throw new Error("This action type isn't supported.");
   }
 }
 
-const initialState = { count: 0, step: 1 };
-
 export default function Counter() {
-  const [count, dispatch] = React.useReducer(reducer, initialState);
+  const [state, dispatch] = React.useReducer(reducer, {
+    count: 0,
+    step: 1,
+  });
 
   const handleIncrement = () => dispatch({ type: "increment" });
   const handleDecrement = () => dispatch({ type: "decrement" });
   const handleReset = () => dispatch({ type: "reset" });
-  const handleUpdateStep = (step) =>
-    dispatch({
-      type: "updateStep",
-      step,
-    });
+  const handleUpdateStep = (step) => dispatch({ type: "updateStep", step });
 
   return (
     <main>
-      <h1>{count}</h1>
+      <h1>{state.count}</h1>
       <div>
-        <button style={{ marginRight: "10px" }} onClick={handleIncrement}>
-          +
+        <button style={{ marginRight: "10px" }} onClick={handleDecrement}>
+          -
         </button>
-        <button onClick={handleDecrement}>-</button>
+        <button onClick={handleIncrement}>+</button>
         <br />
         <br />
         <button onClick={handleReset}>Reset</button>
+        <br />
+        <br />
       </div>
-      <br />
-      <br />
       <Slider min={1} max={10} onChange={handleUpdateStep} />
     </main>
   );
