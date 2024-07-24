62bb0fef665d1471130057842df624506edab90b
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-07-24 18:12:24 +0530

    reduce method

--- react/useReducer/src/jsEx.js
+++ react/useReducer/src/jsEx.js
@@ -4,11 +4,21 @@
 //   console.log(`Hello, ${friend}!`);
 // });
 
+// const numbers = [1, 2, 3, 4, 5];
+
+// let state = 0;
+
+// numbers.forEach((number) => {
+//   state += number;
+// });
+// console.log(state);
+
 const numbers = [1, 2, 3, 4, 5];
+const initialState = 0;
 
-let state = 0;
+function reducer(state, value) {
+  return state + value;
+}
 
-numbers.forEach((number) => {
-  state += number;
-});
-console.log(state);
+const total = numbers.reduce(reducer, initialState);
+console.log(`Total: ${total}`);
