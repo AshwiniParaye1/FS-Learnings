3cd203eae380d0734e08e663a9f84f4124072448
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-07-24 18:01:27 +0530

    adding numbers using forEach

--- react/useReducer/src/jsEx.js
+++ react/useReducer/src/jsEx.js
@@ -1,5 +1,14 @@
-const friends = ["John", "Jane", "Joe", "Jill", "Jim"];
+// const friends = ["John", "Jane", "Joe", "Jill", "Jim"];
 
-friends.forEach((friend) => {
-  console.log(`Hello, ${friend}!`);
+// friends.forEach((friend) => {
+//   console.log(`Hello, ${friend}!`);
+// });
+
+const numbers = [1, 2, 3, 4, 5];
+
+let state = 0;
+
+numbers.forEach((number) => {
+  state += number;
 });
+console.log(state);
