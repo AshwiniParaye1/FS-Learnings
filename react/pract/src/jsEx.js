8bfb73c9d39b3cb9bd8b051d0758366772f8bd21
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-07-28 10:34:53 +0530

    reference value

--- react/pract/src/jsEx.js
+++ react/pract/src/jsEx.js
@@ -1,13 +1,27 @@
-let firstName = "Tyler";
-let lastName = "McGinnis";
-let displayName = `${firstName} ${lastName}`;
+// let firstName = "Tyler";
+// let lastName = "McGinnis";
+// let displayName = `${firstName} ${lastName}`;
 
-function getMarried(newLastName) {
-  lastName = newLastName;
-}
+// function getMarried(newLastName) {
+//   lastName = newLastName;
+// }
 
-getMarried("Brown");
+// getMarried("Brown");
 
-console.log("firstName:", firstName);
-console.log("lastName:", lastName);
-console.log("displayName:", displayName);
+// console.log("firstName:", firstName);
+// console.log("lastName:", lastName);
+// console.log("displayName:", displayName);
+
+let leo = {
+  type: "Dog",
+  age: 0,
+  goodBoy: true,
+  name: "Leo",
+};
+
+let snoop = leo;
+
+snoop.name = "Snoop";
+
+console.log("leo's name:", leo.name);
+console.log("snoop's name:", snoop.name);
