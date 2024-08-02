04aaa3cef75de871dbb7734535a6e0105701c036
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-02 18:22:08 +0530

    any type

--- typescript/node-app/a.js
+++ typescript/node-app/a.js
@@ -1,3 +1,6 @@
 "use strict";
 const x = 1;
 console.log(x);
+// let y: number = 1;
+// y = "ashwini";
+// console.log(y);
--- typescript/node-app/a.ts
+++ typescript/node-app/a.ts
@@ -1,6 +1,20 @@
-const x: number = 1;
-console.log(x);
+// const x: number = 1;
+// console.log(x);
 
-let y: number = 1;
-y = "ashwini";
-console.log(y);
+// let y: number = 1;
+// y = "ashwini";
+// console.log(y);
+
+// - implicitly declared any
+function greet(firstName) {
+  console.log(`Heyy ${firstName}`);
+}
+
+greet("Ashwini");
+
+// explicitly declared any
+function greet1(firstName: any) {
+  console.log(`Heyy ${firstName}`);
+}
+
+greet1("Ashwini");
