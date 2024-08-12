e9bb391ef84585b778e5a6f614ac3d1af331b256
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-12 19:40:59 +0530

    routing

---
+++ auth-next-js/src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center text-4xl">
+      <h1>Login</h1>
+    </div>
+  );
+}
---
+++ auth-next-js/src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SignupPage() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center text-4xl">
+      <h1>Signup</h1>
+    </div>
+  );
+}
