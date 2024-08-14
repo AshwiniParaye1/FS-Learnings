d4acb867ef07e1b072f3b42243bef76b7ccac5b4
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-14 17:39:41 +0530

    user created in db

--- auth-next-js/src/app/signup/page.tsx
+++ auth-next-js/src/app/signup/page.tsx
@@ -1,64 +1,67 @@
 "use client";
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function SignupPage() {
   const router = useRouter();
 
-  const [user, setUser] = React.useState({
+  const [user, setUser] = useState({
     username: "",
     email: "",
     password: "",
   });
 
-  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
 
-  const onSignup = async () => {};
+  const onSignup = async () => {
+    try {
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup successful == ", response.data);
+      toast.success("Signup successful!");
+      router.push("/login");
+    } catch (error) {
+      toast.error("Something went wrong!");
+    }
+  };
 
   useEffect(() => {
-    if (
-      user.username.length > 0 &&
-      user.email.length > 0 &&
-      user.password.length > 0
-    ) {
-      setButtonDisabled(false);
-    } else {
-      setButtonDisabled(true);
-    }
+    setButtonDisabled(!(user.username && user.email && user.password));
   }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Signup</h1>
       <hr />
-      <label htmlFor="username">username</label>
+      <label htmlFor="username">Username</label>
       <input
         id="username"
         type="text"
         value={user.username}
-        placeholder="username"
+        placeholder="Username"
         onChange={(e) => setUser({ ...user, username: e.target.value })}
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black "
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
       />
-      <label htmlFor="email">email</label>
+      <label htmlFor="email">Email</label>
       <input
         id="email"
         type="text"
         value={user.email}
-        placeholder="email"
+        placeholder="Email"
         onChange={(e) => setUser({ ...user, email: e.target.value })}
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black "
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
       />
-      <label htmlFor="password">password</label>
+      <label htmlFor="password">Password</label>
       <input
         id="password"
-        type="text"
+        type="password"
         value={user.password}
-        placeholder="password"
+        placeholder="Password"
         onChange={(e) => setUser({ ...user, password: e.target.value })}
-        className="p-2 border border-gray-300 rounded-lg mb-4 text-black focus:outline-none focus:border-gray-600 "
+        className="p-2 border border-gray-300 rounded-lg mb-4 text-black focus:outline-none focus:border-gray-600"
       />
       <button
         onClick={onSignup}
@@ -67,7 +70,7 @@ export default function SignupPage() {
       >
         Signup Here
       </button>
-
+      <ToastContainer position="top-right" />
       <Link href="/login">Visit Login Page</Link>
     </div>
   );
