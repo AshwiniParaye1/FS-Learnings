7503990f9e29db7039d42dc7ec7d06e7b3d258cd
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-14 18:05:43 +0530

    login route and cookies set

---
+++ auth-next-js/src/app/api/users/login/route.ts
@@ -0,0 +1,60 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel.js";
+import bcryptjs from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const requestBody = await request.json();
+    const { email, password } = requestBody;
+    console.log("requestBody ==== ", requestBody);
+
+    //check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "User does not exist" },
+        { status: 400 }
+      );
+    }
+
+    //check if password is correct
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json(
+        { error: "Invalid credentials" },
+        { status: 400 }
+      );
+    }
+
+    //create JWT token data
+    const tokenData = {
+      _id: user._id,
+      email: user.email,
+      username: user.username,
+    };
+
+    //create JWT token
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1h",
+    });
+
+    const response = NextResponse.json({
+      message: "User logged in successfully",
+      success: true,
+      token,
+    });
+
+    //set JWT token in cookie
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message, status: 500 });
+  }
+}
