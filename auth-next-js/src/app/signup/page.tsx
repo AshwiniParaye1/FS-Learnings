301f8ebdf0f684d66f4096dc632069c96fba6791
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-14 16:15:16 +0530

    created signup form

--- auth-next-js/src/app/signup/page.tsx
+++ auth-next-js/src/app/signup/page.tsx
@@ -1,17 +1,33 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 
 export default function SignupPage() {
+  const router = useRouter();
+
   const [user, setUser] = React.useState({
+    username: "",
     email: "",
     password: "",
-    username: "",
   });
 
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+
   const onSignup = async () => {};
+
+  useEffect(() => {
+    if (
+      user.username.length > 0 &&
+      user.email.length > 0 &&
+      user.password.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -24,7 +40,7 @@ export default function SignupPage() {
         value={user.username}
         placeholder="username"
         onChange={(e) => setUser({ ...user, username: e.target.value })}
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 "
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black "
       />
       <label htmlFor="email">email</label>
       <input
@@ -33,7 +49,7 @@ export default function SignupPage() {
         value={user.email}
         placeholder="email"
         onChange={(e) => setUser({ ...user, email: e.target.value })}
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 "
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black "
       />
       <label htmlFor="password">password</label>
       <input
@@ -42,13 +58,13 @@ export default function SignupPage() {
         value={user.password}
         placeholder="password"
         onChange={(e) => setUser({ ...user, password: e.target.value })}
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 "
+        className="p-2 border border-gray-300 rounded-lg mb-4 text-black focus:outline-none focus:border-gray-600 "
       />
       <button
         onClick={onSignup}
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
       >
-        Signup Here
+        {buttonDisabled ? "Disabled" : "Signup Here"}
       </button>
 
       <Link href="/login">Visit Login Page</Link>
