0497e29b8b48b126ab2e9debeeced8e0224a5031
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-14 19:11:06 +0530

    login api connection done

--- auth-next-js/src/app/login/page.tsx
+++ auth-next-js/src/app/login/page.tsx
@@ -1,16 +1,43 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import { NextResponse } from "next/server";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function LoginPage() {
+  const router = useRouter();
+
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
 
-  const onLogin = async () => {};
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+
+  const onLogin = async () => {
+    try {
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login successful == ", response.data);
+      toast.success("Login successful!");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Something went wrong! ", error);
+
+      toast.error("Something went wrong!");
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+  };
+
+  useEffect(() => {
+    if (user.email && user.password) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -24,7 +51,7 @@ export default function LoginPage() {
         value={user.email}
         placeholder="email"
         onChange={(e) => setUser({ ...user, email: e.target.value })}
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 "
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
       />
       <label htmlFor="password">password</label>
       <input
@@ -33,15 +60,18 @@ export default function LoginPage() {
         value={user.password}
         placeholder="password"
         onChange={(e) => setUser({ ...user, password: e.target.value })}
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 "
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
       />
       <button
         onClick={onLogin}
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className={`p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 ${
+          buttonDisabled && "bg-gray-400 text-gray-600 cursor-not-allowed"
+        }`}
+        disabled={buttonDisabled}
       >
         Login Here
       </button>
-
+      <ToastContainer position="top-right" />
       <Link href="/signup">Visit Signup Page</Link>
     </div>
   );
