6ed672f0409fefe223ccf9fd6e83a0a150cd7660
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-20 20:00:20 +0530

    removed module css

--- next/app/page.jsx
+++ next/app/page.jsx
@@ -1,10 +1,10 @@
-import styles from './page.module.css'
-
 export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className={styles.title}>Next Js 13 starter kit</h1>
+        <h1 className='mt-4 text-3xl font-bold text-green-600'>
+          Next Js 13 starter kit
+        </h1>
       </div>
     </section>
   )
