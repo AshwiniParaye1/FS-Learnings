c32cc78bdc40ba8fa600ab7d1191d474bea33286
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-22 18:09:33 +0530

    added some style to about layout

--- next/app/about/layout.jsx
+++ next/app/about/layout.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function AboutLayout({ children }) {
   return (
-    <section>
-      <h1>AboutLayout</h1>
+    <section className='py-24'>
+      <div className="container border py-4 rounded">
+        <h1 className='text-lg font-serif'>About Layout</h1>
       <div className='mt-6'>{children}</div>
+      </div>
     </section>
   )
 }
