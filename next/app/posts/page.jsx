286756c9649cc3e8d44d6288d7a25fe94d2e1b08
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-22 16:13:37 +0530

    getting all posts

--- next/app/posts/page.jsx
+++ next/app/posts/page.jsx
@@ -1,7 +1,32 @@
-import React from "react"
+import { getAllPosts } from '@/lib/posts'
+import Link from 'next/link'
+import React from 'react'
 
-function Page() {
-  return <div>All blog posts</div>
+const Page = async() => {
+
+  const posts = await  getAllPosts()
+
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='mt-4 text-3xl font-bold'>All Blog Posts</h1>
+        <ul className='mt-12'>
+          {posts.map(post => (
+            <li key={post.slug} className=' mb-2'>
+              <Link href={`/posts/${post.slug}`}>
+              <h4 className='text-lg font-medium'>
+              {post.frontmatter?.title}
+              </h4>
+                <p className='text-sm text-gray-500'>
+                  {post.frontmatter?.author}
+                </p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  )
 }
 
 export default Page
