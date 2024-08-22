1477e1898551cc42e194f6eb619664d88d4e5331
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-22 11:13:10 +0530

    removed footer for now

--- next/app/components/layout/footer.jsx
+++ next/app/components/layout/footer.jsx
@@ -3,9 +3,7 @@ import React from "react"
 function Footer() {
   return (
     <>
-      <footer className='mt-6 bg-gray-100 p-4'>
-        <h2>Footer</h2>
-      </footer>
+      <footer></footer>
     </>
   )
 }
