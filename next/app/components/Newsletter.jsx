b253c7af9ff6d5ef5214fb3a5fb04c3f5d6774d8
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-22 15:27:04 +0530

    passing custom component to mdx

---
+++ next/app/components/Newsletter.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Newsletter = () => {
+  return (
+    <section>
+      <div className='mb-6 rounded bg-blue-400 p-6 text-white'>
+        <p className='text-base font-medium'>Subscribe to my Newsletter</p>
+      </div>
+    </section>
+  )
+}
+
+export default Newsletter
--- next/lib/posts.js
+++ next/lib/posts.js
@@ -1,32 +1,45 @@
 import fs from "fs"
 import path from "path"
 import { compileMDX } from "next-mdx-remote/rsc"
+import Newsletter from "@/app/components/Newsletter"
 
 const rootDirectory = path.join(process.cwd(), "content")
 
 const components = {
-    h1: (props) => (
-        <h1 className="text-3xl font-bold text-green-500 mb-6" {...props} >
-            {props.children}
-        </h1>
-    ),
-    p: (props) => (
-        <p className="mb-4 text-extralight text-gray-500" {...props} >
-            {props.children}
-        </p>
-    ),
-    ul: (props) => (
-        <p className="text-extralight text-gray-500" {...props} >
-            {props.children}
-        </p>
-    ),
+  h1: props => (
+    <h1 className='mb-6 text-3xl font-bold text-green-500' {...props}>
+      {props.children}
+    </h1>
+  ),
+  p: props => (
+    <p className='text-extralight mb-4 text-gray-500' {...props}>
+      {props.children}
+    </p>
+  ),
+  ul: props => (
+    <ul className='text-extralight text-gray-500' {...props}>
+      {props.children}
+    </ul>
+  ),
+  li: props => (
+    <li className='text-extralight text-gray-500' {...props}>
+      {props.children}
+    </li>
+  ),
+  Newsletter: Newsletter
 }
 
 export async function getPostBySlug(slug) {
   const realSlug = slug.replace(/\.mdx$/, "")
-
   const fullPath = path.join(rootDirectory, `${realSlug}.mdx`)
-  const fileContents = fs.readFileSync(fullPath, { encoding: "utf8" })
+
+  let fileContents
+  try {
+    fileContents = fs.readFileSync(fullPath, { encoding: "utf8" })
+  } catch (error) {
+    console.error("Error reading the file", error)
+    throw new Error("Could not read the MDX file")
+  }
 
   const { content, frontmatter } = await compileMDX({
     source: fileContents,
