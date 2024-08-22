0ccf313fd858042f27b136ea64585f22c5815fe6
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-22 11:17:49 +0530

    created nested posts route

---
+++ next/app/posts/learn-nextjs/page.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Page() {
+  return <div>Learn next js</div>
+}
+
+export default Page
---
+++ next/app/posts/learn-react/page.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Page() {
+  return <div>Learn React</div>
+}
+
+export default Page
