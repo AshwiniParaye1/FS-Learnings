25f0641fd0ec15c2af6ed3c7b2acf4977ac576d3
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-11-12 16:10:57 +0530

    hoisitng and functions

--- js/main.js
+++ js/main.js
@@ -1,12 +1,90 @@
-var n = 4;
+// var n = 4;
 
-function sq(num) {
-  var ans = num * num;
-  return ans;
-}
+// function sq(num) {
+//   var ans = num * num;
+//   return ans;
+// }
 
-var sq4 = sq(n);
-var sq8 = sq(8);
+// var sq4 = sq(n);
+// var sq8 = sq(8);
 
-console.log(sq4);
-console.log(sq8);
+// console.log(sq4);
+// console.log(sq8);
+
+/////////////////////////////////////////////////////////////////////////////
+
+// Hoisting
+
+// getName();
+// console.log(a);
+
+// console.log(getName);
+
+// var a = 8;
+
+// function getName() {
+//   console.log("Hello from getName()");
+// }
+
+// getName();
+// console.log(a);
+
+// console.log(getName);
+
+////////////////////////////////////////////////////////////////////////////////
+
+// var a = 8;
+
+// function getName() {
+//   console.log("Hello from getName()");
+// }
+
+// getName();
+// console.log(a);
+
+////////////////////////////////////////////////////////////////////////////////
+
+// getName();
+// console.log(a);
+// console.log(getName);
+
+// var a = 8;
+
+// function getName() {
+//   console.log("Hello from getName()");
+// }
+
+////////////////////////////////////////////////////////////////////////////////
+
+// getName();
+// console.log(a);
+// console.log(getName);
+
+// var a = 8;
+
+// var getName2 = function () {
+//   console.log("func 2");
+// };
+
+// var getName = () => {
+//   console.log("Hello from getName()");
+// };
+
+////////////////////////////////////////////////////////////////////////////////
+
+// var x = 1;
+// a();
+// b();
+// console.log(x);
+
+// function a() {
+//   var x = 10;
+//   console.log(x);
+// }
+
+// function b() {
+//   var x = 100;
+//   console.log(x);
+// }
+
+////////////////////////////////////////////////////////////////////////////////
