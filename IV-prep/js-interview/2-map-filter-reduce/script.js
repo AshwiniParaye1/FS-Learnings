8183d88b316c461a8f14b0958879d42ef49058d9
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-12-27 19:33:38 +0530

    map, filter, reduce

---
+++ IV-prep/js-interview/2-map-filter-reduce/script.js
@@ -0,0 +1,21 @@
+//map, filter, reduce
+
+const nums = [1, 2, 3, 4];
+
+// const multiply3 = nums.map((num) => {
+//   return num * 3;
+// });
+
+// console.log(multiply3);
+
+// const moreThan2 = nums.filter((num) => {
+//   return num > 2;
+// });
+
+// console.log(moreThan2);
+
+// const sum = nums.reduce((acc, curr, i, arr) => {
+//   return acc + curr;
+// }, 0);
+
+// console.log(sum);
