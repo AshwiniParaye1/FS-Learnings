1279b961f41099579ab255f1c8f54fad45732281
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-01-03 09:45:39 +0530

    what is call, bind and apply

--- IV-prep/js-interview/8-call-bind-apply/script.js
+++ IV-prep/js-interview/8-call-bind-apply/script.js
@@ -1,3 +1,55 @@
 // call, bind and apply - explicit binding
 
 /////////////////////////////////////////////////////////////////////////////////////////
+
+//call
+// var obj = { name: "JavaScript" };
+
+// function sayHello(age) {
+//   console.log("Hello " + this.name + " is " + age + " years old");
+// }
+
+// sayHello.call(obj, 30);
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+//apply
+// var obj = { name: "JavaScript" };
+
+// function sayHello(age, profession) {
+//   console.log(
+//     "Hello " +
+//       this.name +
+//       " is " +
+//       age +
+//       " years old" +
+//       " and is a " +
+//       profession
+//   );
+// }
+
+// sayHello.apply(obj, [30, "language"]);
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+//bind
+// var obj = { name: "JavaScript" };
+
+// function sayHello(age, profession) {
+//   return (
+//     "Hello " +
+//     this.name +
+//     " is " +
+//     age +
+//     " years old" +
+//     " and is a " +
+//     profession
+//   );
+// }
+
+// const bindFunc = sayHello.bind(obj);
+
+// console.log(bindFunc(30, "language"));
+// console.log(bindFunc(60, "magical language"));
+
+/////////////////////////////////////////////////////////////////////////////////////////
