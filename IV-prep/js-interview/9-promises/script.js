33cb84672f4be58da52c474ea870f1c982c14a1d
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-01-07 19:07:02 +0530

    o/p  based questions

--- IV-prep/js-interview/9-promises/script.js
+++ IV-prep/js-interview/9-promises/script.js
@@ -281,3 +281,295 @@
 // result();
 
 // console.log("end");
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Q1 - guess the o/p
+
+// console.log("start");
+
+// const promise1 = new Promise((res, rej) => {
+//   console.log(1);
+//   res(2);
+// });
+
+// promise1.then((res) => {
+//   console.log(res);
+// });
+
+// console.log("end");
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Q2 - guess the o/p
+
+// console.log("start");
+
+// const promise1 = new Promise((res, rej) => {
+//   console.log(1);
+//   res(2);
+//   console.log(3);
+// });
+
+// promise1.then((res) => {
+//   console.log(res);
+// });
+
+// console.log("end");
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Q3 - guess the o/p
+
+// console.log("start");
+
+// const promise1 = new Promise((res, rej) => {
+//   console.log(1);
+//   res(2);
+//   console.log(3);
+// });
+
+// promise1.then((res) => {
+//   console.log(res);
+// });
+
+// console.log("end");
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Q4 - guess the o/p
+
+// console.log("start");
+
+// const fn = () =>
+//   new Promise((res, rej) => {
+//     console.log(1);
+//     res("success");
+//   });
+
+// console.log("middle");
+
+// fn().then((res) => {
+//   console.log(res);
+// });
+
+// console.log("end");
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Q5
+
+// function job() {
+//   return new Promise(function (res, rej) {
+//     rej();
+//   });
+// }
+
+// let promise = job();
+
+// promise
+//   .then(function () {
+//     console.log("success 1");
+//   })
+//   .then(function () {
+//     console.log("success 2");
+//   })
+//   .then(function () {
+//     console.log("success 3");
+//   })
+//   .catch(function () {
+//     console.log("Error 1");
+//   })
+//   .then(function () {
+//     console.log("success 4");
+//   });
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Q5
+
+// function job(state) {
+//   return new Promise(function (res, rej) {
+//     if (state) {
+//       res("success");
+//     } else {
+//       rej("error");
+//     }
+//   });
+// }
+
+// let promise = job(true);
+
+// promise
+//   .then(function (data) {
+//     console.log(data);
+
+//     return job(false);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+
+//     return "error caught";
+//   })
+//   .then(function (data) {
+//     console.log(data);
+
+//     return job(true);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Q6 -
+
+// function job(state) {
+//   return new Promise(function (res, rej) {
+//     if (state) {
+//       res("success");
+//     } else {
+//       rej("error");
+//     }
+//   });
+// }
+
+// let promise = job(true);
+
+// promise
+//   .then(function (data) {
+//     console.log(data);
+
+//     return job(true);
+//   })
+//   .then(function (data) {
+//     if (data !== "victory") {
+//       throw "Defeat"; //throwing an error
+//     }
+
+//     return job(true);
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+
+//     return job(false);
+//   })
+//   .then(function (data) {
+//     console.log(data);
+
+//     return job(true);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+
+//     return "error caught";
+//   })
+//   .then(function (data) {
+//     console.log(data);
+
+//     return new Error("test error");
+//   })
+//   .then(function (data) {
+//     console.log("success test:", data.message);
+//   });
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Q7 - create a promise called firstPromise which will resolve to a text called 'first' and then we have to create another promise called secondPromise which will resolve out firstPromise which we had created earlier
+
+// we have to resolve out secondPromise and output of which will be passed to firstPromise and then print the first promise
+
+// const firstPromise = new Promise((res, rej) => {
+//   res("first!");
+// });
+
+// const secondPromise = new Promise((res, rej) => {
+//   res(firstPromise);
+// });
+
+// secondPromise
+//   .then((res) => {
+//     return res;
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   });
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Q8 - rewrite using async await
+
+// async function loadJson(url) {
+//   const response = await fetch(url);
+
+//   if (response.status == 200) {
+//     const json = await response.json();
+//     console.log(json.results[0]);
+
+//     return json;
+//   }
+
+//   throw new Error(response.status);
+
+//      return fetch(url).then((res) => {
+//       if (res.status == 200) {
+//          return res.json();
+//        }
+//        throw new Error(res.status);
+//      });
+// }
+
+// loadJson("https://randomuser.me/api/").catch((err) => {
+//   console.log("something went wrong!", err);
+// });
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Q9 - solve promise recursively
+
+// function impAction(username) {
+//   return new Promise((res, rej) => {
+//     setTimeout(() => {
+//       res(`Hey! I am ${username}`);
+//     }, 1000);
+//   });
+// }
+
+// function likeTheVideo(video) {
+//   return new Promise((res, rej) => {
+//     setTimeout(() => {
+//       res(`Like the ${video} video`);
+//     }, 1000);
+//   });
+// }
+
+// function shareTheVideo(video) {
+//   return new Promise((res, rej) => {
+//     setTimeout(() => {
+//       res(`Share the ${video} video`);
+//     }, 1000);
+//   });
+// }
+
+// function promRecursively(funcPromises) {
+//   if (funcPromises.length === 0) return;
+
+//   const currPromise = funcPromises.shift();
+
+//   currPromise
+//     .then((res) => console.log(res))
+//     .catch((err) => console.error(err));
+
+//   promRecursively(funcPromises);
+// }
+
+// promRecursively([
+//   impAction("JavaScript"),
+//   likeTheVideo("JavaScript interview question"),
+//   shareTheVideo("JavaScript interview question")
+// ]);
+
+/////////////////////////////////////////////////////////////////////////////////////////
