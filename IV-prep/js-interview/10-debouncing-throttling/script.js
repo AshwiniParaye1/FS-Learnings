1970c9614f711a268928e7e718acd7f79dac1c4c
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-01-08 10:55:05 +0530

    Throttle() polyfill

--- IV-prep/js-interview/10-debouncing-throttling/script.js
+++ IV-prep/js-interview/10-debouncing-throttling/script.js
@@ -52,6 +52,38 @@
 
 // Q3  - create Debounce() polyfill implementation
 
+// const btn = document.querySelector(".increment-btn");
+// const btnPress = document.querySelector(".increment-pressed");
+// const count = document.querySelector(".increment-count");
+
+// var pressedCount = 0;
+// var triggeredCount = 0;
+
+// const myDebounce = (cb, waitTime) => {
+//   let timer;
+
+//   return function (...args) {
+//     if (timer) clearTimeout(timer);
+
+//     timer = setTimeout(() => {
+//       cb(...args);
+//     }, waitTime);
+//   };
+// };
+
+// var debounceCount = myDebounce(() => {
+//   count.innerHTML = ++triggeredCount;
+// }, 800);
+
+// btn.addEventListener("click", () => {
+//   btnPress.innerHTML = ++pressedCount;
+//   debounceCount();
+// });
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Q4  - create Throttle() polyfill implementation
+
 const btn = document.querySelector(".increment-btn");
 const btnPress = document.querySelector(".increment-pressed");
 const count = document.querySelector(".increment-count");
@@ -59,23 +91,32 @@ const count = document.querySelector(".increment-count");
 var pressedCount = 0;
 var triggeredCount = 0;
 
-const myDebounce = (cb, waitTime) => {
-  let timer;
+const start = new Date().getTime();
 
-  return function (...args) {
-    if (timer) clearTimeout(timer);
+const myThrottle = (cb, waitTime) => {
+  let last = 0;
 
-    timer = setTimeout(() => {
-      cb(...args);
-    }, waitTime);
+  return (...args) => {
+    let now = new Date().getTime();
+
+    if (now - last < waitTime) return;
+
+    last = now;
+
+    return cb(...args);
   };
 };
 
-var debounceCount = myDebounce(() => {
+var throttledCount = myThrottle(() => {
   count.innerHTML = ++triggeredCount;
-}, 800);
+}, 1000);
 
 btn.addEventListener("click", () => {
   btnPress.innerHTML = ++pressedCount;
-  debounceCount();
+
+  const now = new Date().getTime();
+  const seconds = (now - start) / 1000;
+  console.log(seconds.toFixed());
+
+  throttledCount();
 });
