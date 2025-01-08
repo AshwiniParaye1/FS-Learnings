66d767feea1e683a3f62c3dc9d88360928c2f756
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-01-08 10:33:22 +0530

    throttle using lodash

--- IV-prep/js-interview/10-debouncing-throttling/script.js
+++ IV-prep/js-interview/10-debouncing-throttling/script.js
@@ -2,9 +2,35 @@
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
-// Q1 - create a button UI and add debounce as follows =>
+// debounce using lodash
+
+// Q1 - create a button UI and add throttle as follows =>
 //    --> show button pressed <X> times every time button is pressed
-//    --> increase triggered  <Y> times count after 800ms of debounce
+//    --> increase triggered  <Y> times count after 800ms of throttle
+
+// const btn = document.querySelector(".increment-btn");
+// const btnPress = document.querySelector(".increment-pressed");
+// const count = document.querySelector(".increment-count");
+
+// var pressedCount = 0;
+// var triggeredCount = 0;
+
+// var debounceCount = _.debounce(() => {
+//   count.innerHTML = ++triggeredCount;
+// }, 800);
+
+// btn.addEventListener("click", () => {
+//   btnPress.innerHTML = ++pressedCount;
+//   debounceCount();
+// });
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// throttle using lodash
+
+// Q2  - create a button UI and add throttle as follows =>
+//    --> show button pressed <X> times every time button is pressed
+//    --> increase triggered  <Y> times count after 800ms of throttle
 
 const btn = document.querySelector(".increment-btn");
 const btnPress = document.querySelector(".increment-pressed");
@@ -13,17 +39,13 @@ const count = document.querySelector(".increment-count");
 var pressedCount = 0;
 var triggeredCount = 0;
 
-var debounceCount = _.debounce(() => {
+var throttleCount = _.throttle(() => {
   count.innerHTML = ++triggeredCount;
 }, 800);
 
 btn.addEventListener("click", () => {
   btnPress.innerHTML = ++pressedCount;
-  debounceCount();
+  throttleCount();
 });
 
 /////////////////////////////////////////////////////////////////////////////////////////
-
-// Q2 -
-//    -->
-//    -->
