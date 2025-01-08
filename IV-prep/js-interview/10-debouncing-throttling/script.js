4c53f3d4a2fb266e09c3e8eb32ff938f71598e5f
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-01-08 10:55:23 +0530

    Throttle() polyfill

--- IV-prep/js-interview/10-debouncing-throttling/script.js
+++ IV-prep/js-interview/10-debouncing-throttling/script.js
@@ -84,39 +84,39 @@
 
 // Q4  - create Throttle() polyfill implementation
 
-const btn = document.querySelector(".increment-btn");
-const btnPress = document.querySelector(".increment-pressed");
-const count = document.querySelector(".increment-count");
+// const btn = document.querySelector(".increment-btn");
+// const btnPress = document.querySelector(".increment-pressed");
+// const count = document.querySelector(".increment-count");
 
-var pressedCount = 0;
-var triggeredCount = 0;
+// var pressedCount = 0;
+// var triggeredCount = 0;
 
-const start = new Date().getTime();
+// const start = new Date().getTime();
 
-const myThrottle = (cb, waitTime) => {
-  let last = 0;
+// const myThrottle = (cb, waitTime) => {
+//   let last = 0;
 
-  return (...args) => {
-    let now = new Date().getTime();
+//   return (...args) => {
+//     let now = new Date().getTime();
 
-    if (now - last < waitTime) return;
+//     if (now - last < waitTime) return;
 
-    last = now;
+//     last = now;
 
-    return cb(...args);
-  };
-};
+//     return cb(...args);
+//   };
+// };
 
-var throttledCount = myThrottle(() => {
-  count.innerHTML = ++triggeredCount;
-}, 1000);
+// var throttledCount = myThrottle(() => {
+//   count.innerHTML = ++triggeredCount;
+// }, 1000);
 
-btn.addEventListener("click", () => {
-  btnPress.innerHTML = ++pressedCount;
+// btn.addEventListener("click", () => {
+//   btnPress.innerHTML = ++pressedCount;
 
-  const now = new Date().getTime();
-  const seconds = (now - start) / 1000;
-  console.log(seconds.toFixed());
+//   const now = new Date().getTime();
+//   const seconds = (now - start) / 1000;
+//   console.log(seconds.toFixed());
 
-  throttledCount();
-});
+//   throttledCount();
+// });
