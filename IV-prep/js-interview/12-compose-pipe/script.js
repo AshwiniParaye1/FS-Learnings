11b53012c2e019f229b21189d40e7c8ef9a6610c
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-01-17 16:02:03 +0530

    Compose & Pipe

--- IV-prep/js-interview/12-compose-pipe/script.js
+++ IV-prep/js-interview/12-compose-pipe/script.js
@@ -2,27 +2,49 @@
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
-const addFive = (num) => {
-  return num + 5;
-};
-const subtractTwo = (num) => {
-  return num - 2;
-};
-const multiplyFour = (num) => {
-  return num * 4;
-};
+// const addFive = (num) => {
+//   return num + 5;
+// };
+// const subtractTwo = (num) => {
+//   return num - 2;
+// };
+// const multiplyFour = (num) => {
+//   return num * 4;
+// };
 
-const compose = (...fn) => {
-  return function (initial) {
-    let result = initial;
-    for (let i = fn.length - 1; i >= 0; i--) {
-      result = fn[i](result);
-    }
+// const compose = (...fns) => {
+//   //   return function (initial) {
+//   //     let result = initial;
+//   //     for (let i = fns.length - 1; i >= 0; i--) {
+//   //       result = fns[i](result);
+//   //     }
+//   //     return result;
+//   //   };
 
-    return result;
-  };
-};
+//   return function (init) {
+//     return fns.reduceRight((acc, curr) => {
+//       return curr(acc);
+//     }, init);
+//   };
+// };
 
-const evaluate = compose(addFive, subtractTwo, multiplyFour);
+// const pipe = (...fns) => {
+//   //   return function (init) {
+//   //     let result = init;
+//   //     for (let i = 0; i < fns.length; i++) {
+//   //       result = fns[i](result);
+//   //     }
+//   //     return result;
+//   //   };
+//   return function (init) {
+//     return fns.reduce((acc, curr) => {
+//       return curr(acc);
+//     }, init);
+//   };
+// };
 
-console.log(evaluate(5));
+// const evaluateCompose = compose(addFive, subtractTwo, multiplyFour);
+// const evaluatePipe = pipe(addFive, subtractTwo, multiplyFour);
+
+// console.log(evaluateCompose(5));
+// console.log(evaluatePipe(5));
