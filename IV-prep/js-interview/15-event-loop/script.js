6643c738983a4ad05b54b18bc5881bfa28fe2b38
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-01-17 18:06:18 +0530

    Event Loop examples

--- IV-prep/js-interview/15-event-loop/script.js
+++ IV-prep/js-interview/15-event-loop/script.js
@@ -1,3 +1,46 @@
 //TODO - Event Loop
 
 /////////////////////////////////////////////////////////////////////////////////////////
+
+// console.log("1");
+// console.log("2");
+// console.log("3");
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// console.log("1");
+
+// setTimeout(() => {
+//   console.log("2");
+// }, 2000);
+
+// console.log("3");
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// console.log("start");
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((res) => res.json())
+//   .then(function a(response) {
+//     console.log("Fetch completed:", response[0]);
+//   })
+//   .catch(function c(error) {
+//     console.error("Fetch error:", error);
+//   });
+
+// console.log("middle");
+
+// Promise.resolve().then(function b() {
+//   console.log("Promise resolved");
+// });
+
+// console.log("middle 2");
+
+// Promise.reject().catch(function c() {
+//   console.log("Promise rejected");
+// });
+
+// console.log("end");
+
+/////////////////////////////////////////////////////////////////////////////////////////
