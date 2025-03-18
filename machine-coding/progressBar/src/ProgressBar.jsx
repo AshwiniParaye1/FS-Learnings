b5e9ca64657de20a79ece7ccabcf1366c675c6e7
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-03-18 19:59:40 +0530

    added animation

--- machine-coding/progressBar/src/ProgressBar.jsx
+++ machine-coding/progressBar/src/ProgressBar.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ProgressBar = ({ progress }) => {
+  const [animatedProgress, setAnimatedProgress] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimatedProgress(progress);
+    }, 100);
+  }, [progress]);
+
   return (
     <div className="progressBarContainer">
       <div
         className="progressBar"
         style={{
-          width: `${progress}%`,
+          //   width: `${progress}%`,
+          transform: `translateX(${animatedProgress - 100}%)`,
           color: progress < 5 ? "black" : "white"
         }}
         role="progressbar"
