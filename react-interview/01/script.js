d2fff63afb5c872a5520277df3f860b7a590f348
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-03-29 11:41:13 +0530

    rendering with variables and  state

--- react-interview/01/script.js
+++ react-interview/01/script.js
@@ -1,15 +1,28 @@
 function Counter() {
   const [count, setCount] = React.useState(0);
 
+  let count1 = 0;
+
   function increment() {
-    setCount(count + 1);
+    // setCount(count + 1);
+    count1 = count1 + 1;
   }
 
-  return React.createElement(
-    "div",
-    null,
-    React.createElement("p", null, `Count: ${count}`),
-    React.createElement("button", { onClick: increment }, `Increment`)
+  //   return React.createElement(
+  //     "div",
+  //     null,
+  //     React.createElement("p", null, `Count: ${count}`),
+  //     React.createElement("button", { onClick: increment }, `Increment`)
+  //   );
+
+  console.log("counter rendered");
+
+  return (
+    <div>
+      {/* <p>Count: {count}</p> */}
+      <p>Count: {count1}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
   );
 }
 
