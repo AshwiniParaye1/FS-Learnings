f777038e7870b343d56b891ced03c23193116ace
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-03-29 19:37:31 +0530

    rendering react

--- react-interview/01/script.js
+++ react-interview/01/script.js
@@ -1,11 +1,21 @@
 function Counter() {
   const [count, setCount] = React.useState(0);
 
-  let count1 = 0;
+  //   let count1 = 0;
 
   function increment() {
+    // count1 = count1 + 1;
     // setCount(count + 1);
-    count1 = count1 + 1;
+    // setCount(count + 1);
+    // setCount(count + 1);
+    // setCount(count + 1);
+    // setCount(count + 1);
+
+    setCount((prevCount) => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
   }
 
   //   return React.createElement(
@@ -19,13 +29,37 @@ function Counter() {
 
   return (
     <div>
-      {/* <p>Count: {count}</p> */}
-      <p>Count: {count1}</p>
+      <p>Count: {count}</p>
+      {/* <p>Count: {count1}</p> */}
       <button onClick={increment}>Increment</button>
     </div>
   );
 }
 
+const CounterParent = () => {
+  const [showMsg, setShowMsg] = React.useState(false);
+  const [showMsg2, setShowMsg2] = React.useState(false);
+
+  const toggleMsg = () => {
+    setShowMsg(!showMsg);
+    setShowMsg2(!showMsg2);
+  };
+
+  console.log("Parent rendered");
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <Counter />
+      <br />
+      {showMsg && <b>Now you see me </b>}
+      <br />
+      {showMsg2 && <b>Now you see me Again </b>}
+      <button onClick={toggleMsg}>Show Message</button>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(React.createElement(Counter));
+root.render(React.createElement(CounterParent));
